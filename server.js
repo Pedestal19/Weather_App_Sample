b6259cd84a1461153b6d16a15f5f11cb146ad6a5
Pedestal19 <gabeoji01@gmail.com>
2020-05-15 02:07:47 +0100

    coded logic for server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors())
+
 
 // Initialize the main project folder
-app.use(express.static('website'));
+const siteFolder = 'website';
+app.use(express.static(siteFolder));
+
+//Get Endpoint
+const resourceOkStatusCode = 200;
+const getUrl = '/api/v1/projectdata';
+app.get(getUrl, (request, response) => {
+    response.status(resourceOkStatusCode).send(projectData)
+})
+
+const resourceCreatedStatusCode = 201;
+const postUrl = '/api/v1/projectdata';
+app.post(postUrl, (request, response) => {
+recordToSave = {
+    temperature: request.body.temperature,
+    date: request.body.date,
+    userResponse: request.body.userResponse
+    };
+
+    response.status(resourceCreatedStatusCode).send("resource created/record saved")
+})
 
 
 // Setup Server
+const port = 7777;
+app.listen(port, () => {
+    console.log(`Running Node Server on Port: ${port}`)
+  })
